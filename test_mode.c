aa9cc8c978658abedefb72a3f14fd62063f71c7b
KAMTA KESHARWANI <84734976+Kamta733@users.noreply.github.com>
2021-06-20 12:10:53 +0530

    Update test_mode.c

--- test_mode.c
+++ test_mode.c
@@ -13,25 +13,4 @@ int main(void) {
   printf("Mode: passed\n");
   }
 
-int mode(int a[],int n)
-{
-    int mode=0,maxcount=0;
-    for(int i=0;i<n;i++)
-    {
-        int count=0;
-        for(int j=0;j<n;j++)
-            {
-                if(a[j]==a[i])
-                {
 
-                    count++;
-                }
-            }
-            if(count>maxcount)
-            {
-                maxcount=count;
-                mode=a[i];
-            }
-    }
-    return mode;
-}
